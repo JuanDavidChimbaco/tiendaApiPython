38f406c1a6f84e9f11d78d406efc93ffb0dcdccd
Juan David <dajun318@gmail.com>
2023-08-08 10:04:09 -0500

    cambios a modelos y a serializer para agregar proFoto

--- GestionTienda/appTienda/models.py
+++ GestionTienda/appTienda/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Categoria(models.Model):
     catNombre = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__ (self):
         return self.catNombre
@@ -13,6 +15,9 @@ class Producto(models.Model):
     proNombre = models.CharField(max_length=50,null=False)
     proPrecio = models.IntegerField(null=False)
     proCategoria = models.ForeignKey (Categoria, on_delete=models.PROTECT)
+    proFoto = models.FileField(upload_to='fotos/', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__ (self):
         return self.proNombre
--- GestionTienda/appTienda/serializers.py
+++ GestionTienda/appTienda/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 from appTienda.models import Categoria,Producto
 
 class Categoriaserializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class Categoriaserializer(serializers.ModelSerializer):
         fields = ('id','catNombre')
 
 class Productoserializer(serializers.ModelSerializer):
+    proFoto = Base64ImageField(required=False)
     class Meta:
         model = Producto
         fields = '__all__'
